cedf0f9317790a5950c36d1fa15b1b60346f987f
wili <wilirahmatm@gmail.com>
2019-01-15 11:19:22 +0800

    vue change page script ok

--- resources/views/indexhome.blade.php
+++ resources/views/indexhome.blade.php
@@ -10,15 +10,20 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/animate.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lemon">
 
     <title>Pringgajurang</title>
   </head>
   <body onload="startTime()">
     <!-- Image and text -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+
+<div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
    <div class="container">
-   <a class="navbar-brand" href="#">
+   <a class="navbar-brand" href="#" @click="currentView='indexpage';activate(0);">
     <img src="/images/kabupaten-lombok-timur-ntb.png" width="55" height="70" class="d-inline-block align-top" alt="">
     <div id="spacetextlogo">
     <span id="logotext">Pringgajurang</span>
@@ -32,16 +37,16 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Bidang PEMERINTAHAN</a>
+        <a class="nav-link" href="#" @click="currentView='bidangpemerintahan';activate(1);" :class="{ active : active_el == 1 }">Bidang PEMERINTAHAN</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Panduan PENDUDUK</a>
+        <a class="nav-link" href="#" @click="currentView='panduanpenduduk';activate(2);" :class="{ active : active_el == 2 }">Panduan PENDUDUK</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Halaman BISNIS</a>
+        <a class="nav-link" href="#" @click="currentView='halamanbisnis';activate(3);" :class="{ active : active_el == 3 }">Halaman BISNIS</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Untuk PENGUNJUNG</a>
+        <a class="nav-link" href="#" @click="currentView='untukpengunjung';activate(4);" :class="{ active : active_el == 4 }">Untuk PENGUNJUNG</a>
       </li>
       <li class="nav-item dropdown">
         <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -56,100 +61,170 @@
   </div>
    </div>
 </nav>
+    <component :is="currentView" class="wow slideInLeft" keep-alive></component>
+</div>
+
+<template id="indexpage">
+    <div>
+            <section id="kotak1">
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="tentang_desa inner">
+                            <h2>Tentang Desa</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus et voluptatem aliquid, culpa doloribus illum consectetur omnis quia autem? Ipsa maxime facilis saepe eveniet laboriosam, ducimus, commodi aspernatur quam neque.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="berita_terbaru inner">
+                            <h2>Berita Terbaru</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus et voluptatem aliquid, culpa doloribus illum consectetur omnis quia autem? Ipsa maxime facilis saepe eveniet laboriosam, ducimus, commodi aspernatur quam neque.</p>
+                            <br>
+                            <p class="text-right">admin,21/1/2019</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="pengumuman inner">
+                            <h2>Pengumuman</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus et voluptatem aliquid, culpa doloribus illum consectetur omnis quia autem? Ipsa maxime facilis saepe eveniet laboriosam, ducimus, commodi aspernatur quam neque.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+
+            <section id="kotak2">
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="inner">
+                            <img src="/images/ntp-pangan.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="inner2">
+                            <img src="/images/Screenshot from 2018-10-16 17-11-07.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="inner">
+                            <img src="/images/Grafik-Bonus-Demografi.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="inner2">
+                            <img src="/images/kopi.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+
+            <section id="kotak3">
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-2 offset-md-1">
+                        <div class="inner text-center">
+                            <i class="fas fa-sitemap"></i><br>
+                            <h4><strong>LEMBAGA</strong></h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="inner text-center">
+                            <i class="fas fa-users"></i><br>
+                            <h4><strong>SOTK</strong></h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="inner text-center">
+                            <i class="fas fa-map-marked-alt"></i><br>
+                            <h4><strong>PETA</strong></h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="inner text-center">
+                            <i class="fas fa-calendar-alt"></i><br>
+                            <h4><strong>AGENDA</strong></h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="inner text-center">
+                            <i class="fas fa-chart-bar"></i><br>
+                            <h4><strong>DATA</strong></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+        </div>
+</template>
 
 
-<div id="pagechange">
-    <section id="kotak1">
-    <div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="tentang_desa inner">
-                <h2 class="text-center">Tentang Desa</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus et voluptatem aliquid, culpa doloribus illum consectetur omnis quia autem? Ipsa maxime facilis saepe eveniet laboriosam, ducimus, commodi aspernatur quam neque.</p>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="berita_terbaru inner">
-                <h2 class="text-center">Berita Terbaru</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus et voluptatem aliquid, culpa doloribus illum consectetur omnis quia autem? Ipsa maxime facilis saepe eveniet laboriosam, ducimus, commodi aspernatur quam neque.</p>
-                <br>
-                <p class="text-right">admin,21/1/2019</p>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="pengumuman inner">
-                <h2 class="text-center">Pengumuman</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus et voluptatem aliquid, culpa doloribus illum consectetur omnis quia autem? Ipsa maxime facilis saepe eveniet laboriosam, ducimus, commodi aspernatur quam neque.</p>
+<template id="bidangpemerintahan">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="kotak1bidangpemerintahan">
+                        <ul>
+                        <li>Pemerintahan</li>
+                        <li>Kesejahteraan</li>
+                        <li>Pelayanan</li>
+                        <li>Tata Usaha dan Umum</li>
+                        <li>Keuangan</li>
+                        <li>Perencanaan</li>
+                        <li>Kewilayahan</li>
+                        <li>Pembangunan</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="kotak2bidangpemerintahan">
+                      <ul>
+                        <li>Pemerintahan</li>
+                        <li>Kesejahteraan</li>
+                        <li>Pelayanan</li>
+                        <li>Tata Usaha dan Umum</li>
+                        <li>Keuangan</li>
+                        <li>Perencanaan</li>
+                        <li>Kewilayahan</li>
+                        <li>Pembangunan</li>
+                    </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</section>
+</template>
 
-<section id="kotak2">
-    <div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="inner">
-                <img src="/images/ntp-pangan.jpg" alt="">
+<template id="panduanpenduduk">
+         <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="kotakpanduanpenduduk">
+                        <h1>Panduan Penduduk</h1>
+                        <form action="{{url('caripanduanpenduduk')}}" method="post" accept-charset="utf-8">
+                              {{ csrf_field() }}
+                              <input type="text"  id="search" name="search" placeholder="cari panduan" >
+                              <input type="submit" value="Search" id="tombol_search_panduan">
+                          </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-md-2">
-            <div class="inner2">
-                <img src="/images/Screenshot from 2018-10-16 17-11-07.jpg" alt="">
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="inner">
-                <img src="/images/Grafik-Bonus-Demografi.jpg" alt="">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="inner2">
-                <img src="/images/kopi.jpg" alt="">
-            </div>
-        </div>
-    </div>
-</div>
-</section>
+</template>
 
-<section id="kotak3">
-    <div class="container">
-    <div class="row">
-        <div class="col-md-2 offset-md-1">
-            <div class="inner text-center">
-                <i class="fas fa-sitemap"></i><br>
-                <h4><strong>LEMBAGA</strong></h3>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="inner text-center">
-                <i class="fas fa-users"></i><br>
-                <h4><strong>SOTK</strong></h3>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="inner text-center">
-                <i class="fas fa-map-marked-alt"></i><br>
-                <h4><strong>PETA</strong></h3>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="inner text-center">
-                <i class="fas fa-calendar-alt"></i><br>
-                <h4><strong>AGENDA</strong></h3>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="inner text-center">
-                <i class="fas fa-chart-bar"></i><br>
-                <h4><strong>DATA</strong></h3>
-            </div>
-        </div>
+<template id="halamanbisnis">
+    <div>
+        <h1>halaman bisnis</h1>
     </div>
-</div>
-</section>
-</div>
+</template>
+
+<template id="untukpengunjung">
+    <div>
+        <h1>untuk pengunjung</h1>
+    </div>
+</template>
 
 <footer>
     <div class="container-fluid">
@@ -180,6 +255,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="/js/date.js" type="text/javascript" charset="utf-8" async defer></script>
+    <script src="{{ asset('/js/date.js') }}" type="text/javascript" charset="utf-8" async defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js"></script>
+    <script src="{{ asset('/vue/VueIndex.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('/js/script.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('/js/wow.min.js') }}" type="text/javascript"></script>
   </body>
 </html>
