17e04632758fb890cbde9696ca47a8d2409248ee
wili <wilirahmatm@gmail.com>
2019-01-24 16:33:05 +0800

    debugging responsive 2

--- resources/views/adminkadus.blade.php
+++ resources/views/adminkadus.blade.php
@@ -11,74 +11,69 @@
     <!-- Title -->
     <title>Desa Pringgajurang</title>
     <!-- Place favicon.ico in the root directory -->
-    <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
-    <link rel="shortcut icon" type="image/ico" href="{{asset('images/favicon.ico')}}" />
-    <!-- Plugin-CSS -->
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
-    <!-- Main-Stylesheets -->
-    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('css/responsiveadmin.css')}}">
-    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
-    <link rel="stylesheet" href="{{asset('css/table.css')}}">
-    <script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-    <!--[if lt IE 9]>
-        <script src="{{asset('')}}//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="{{asset('')}}//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/responsive.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/admin.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/responsiveadmin.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/table.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lemon">
 </head>
 
 <body onload="startTime()" data-spy="scroll" data-target="#primary-menu">
 
 
 
-    <!--Mainmenu-area-->
-    <div class="mainmenu-area" data-spy="affix" data-offset-top="100">
-        <div class="container">
-            <!--Logo-->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-menu">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="{{url('/')}}" class="navbar-brand logo">
-                  <img id="logo" src="images/kabupaten-lombok-timur-ntb (1).png" alt="">
-                    <h5><strong>Website Resmi Pemerintah</strong></h5>
-                    <h2>Desa Pringgajurang</h2>
-                </a>
-            </div>
-            <!--Logo/-->
-            <nav class="collapse navbar-collapse" id="primary-menu">
-                <ul class="nav navbar-nav navbar-right">
-                    @guest
-                          <li>
-                              <a href="{{ route('login') }}">{{ __('Login') }}</a>
-                          </li>
-                    @else
-                          
-                          <li>
-                               <a class="dropdown-item" href="{{ route('logout') }}"
-                                                     onclick="event.preventDefault();
-                                                                   document.getElementById('logout-form').submit();">
-                                      {{ __('Logout') }}
-                                  </a>
-
-                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                      @csrf
-                                  </form>
-                          </li>
-                      @endguest
-                </ul>
-            </nav>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <div class="container">
+   <a class="navbar-brand" href="#" @click="currentView='indexpage';activate(0);isHidden = false">
+    <img src="/images/kabupaten-lombok-timur-ntb.png" width="55" height="70" class="d-inline-block align-top" alt="">
+    <div id="spacetextlogo">
+    <span id="logotext">Pringgajurang</span>
+    <span id="logotext2">Website Resmi Pemerintah Desa</span>
     </div>
-    <!--Mainmenu-area/-->
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      @guest
+              <li>
+                  <a href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+        @else
+              <li class="nav-item dropdown">
+                   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->email }} <span class="caret"></span>
+                  </a>
+
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                                         onclick="event.preventDefault();
+                                                       document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+          @endguest
+       </ul>
+    </ul>
+  </div>
+   </div>
+</nav>
+
+
+
+            
 
 
 
@@ -319,18 +314,15 @@
 
 
 
-    <!--Vendor-JS-->
-    <script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
-    <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
-    <!--Plugin-JS-->
-    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
-    <script src="{{asset('js/contact-form.js')}}"></script>
-    <script src="{{asset('js/jquery.parallax-1.1.3.js')}}"></script>
-    <script src="{{asset('js/scrollUp.min.js')}}"></script>
-    <script src="{{asset('js/magnific-popup.min.js')}}"></script>
-    <script src="{{asset('js/wow.min.js')}}"></script>
-    <!--Main-active-JS-->
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{ asset('/js/date.js') }}" type="text/javascript" charset="utf-8" async defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js"></script>
+    {{-- <script src="{{ asset('/vue/VueIndex.js') }}" type="text/javascript"></script> --}}
+    <script src="{{ asset('/js/script.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('/js/wow.min.js') }}" type="text/javascript"></script>
 </body>
 
 </html>
