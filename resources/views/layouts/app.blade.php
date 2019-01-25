e112ba132f3f91d39240823cba1c27559335011d
wili <wilirahmatm@gmail.com>
2019-01-25 14:32:01 +0800

    debugging responsive 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
     <!-- Plugin-CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lemon">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/loginregister.css')}}">
     <link rel="stylesheet" href="{{asset('css/responsiveloginregister.css')}}">
     
@@ -62,7 +62,7 @@
             @yield('content')
         </main>
 
-<footer>
+<footer class="mt-auto">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-4 text-light text-left">
