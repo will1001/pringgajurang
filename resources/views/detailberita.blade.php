8e8fe66af36a4d81307c79f07901ba43fdf7c91c
will1001 <wilirahmatm@gmail.com>
2019-02-13 17:34:55 +0800

    penambahan url video pada tabel berita 9

--- resources/views/detailberita.blade.php
+++ resources/views/detailberita.blade.php
@@ -10,11 +10,12 @@
       <div class="row">
         <div class="col-md-12">
             <img src="{{ $beritas->urlgambar }}" alt="" width="400px" height="250px">
-            <h3></h3>
-            <p style="white-space: pre-line;white-space: pre-wrap;">{{$beritas->deskripsi }}</p><br>
-             <iframe width="420" height="315"
+            <iframe width="420" height="315"
             src="https://www.youtube.com/embed/{{$beritas->urlvideo}}">
             </iframe>
+            <h3>{{$beritas->judulberita }}</h3>
+            <p style="white-space: pre-line;white-space: pre-wrap;">{{$beritas->deskripsi }}</p><br>
+             
         </div>
 
       </div>
