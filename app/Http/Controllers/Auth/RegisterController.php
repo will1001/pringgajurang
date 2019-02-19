e94d2ef8f01d18d7888b1365c6586dc66b9e30d6
will1001 <wilirahmatm@gmail.com>
2019-02-19 11:52:45 +0800

    perbaiki register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             // 'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'Nomor_KK' => 'required|string|max:255|unique:Nomor_KK',
+            'Nomor_KK' => 'required|string|max:255',
             'Alamat' => 'required|string|max:255',
         ]);
     }
