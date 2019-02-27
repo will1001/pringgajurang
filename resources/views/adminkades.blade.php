7eeeb17fe5d606c79cc4f06150a062748740c1b1
will1001 <wilirahmatm@gmail.com>
2019-02-27 12:00:44 +0800

    fix bug adminkades 7

--- resources/views/adminkades.blade.php
+++ resources/views/adminkades.blade.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/admin.css">
-    <link rel="stylesheet" type="text/css" href="/css/responsiveadmin.css">
     <link rel="stylesheet" type="text/css" href="/css/table.css">
 
     <title>Pringgajurang</title>
