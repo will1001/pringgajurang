05fb158cf8671652637167b7f235b56d93f39c43
will1001 <wilirahmatm@gmail.com>
2019-03-01 06:05:53 +0800

    fix bug admin kades 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -38,7 +38,6 @@ import bidangpemerintahan from './components/indexcomponent/BidangPemerintahan.v
 import panduanpenduduk from './components/indexcomponent/PanduanPenduduk.vue';
 import halamanbisnis from './components/indexcomponent/HalamanBisnis.vue';
 import untukpengunjung from './components/indexcomponent/UntukPengunjung.vue';
-import lembagaindex from './components/indexcomponent/LembagaIndex.vue';
 import agenda from './components/indexcomponent/Agenda.vue';
 import datadesa from './components/indexcomponent/DataDesa.vue';
 
@@ -67,7 +66,6 @@ const app = new Vue({
       panduanpenduduk,
       halamanbisnis,
       untukpengunjung,
-      lembagaindex,
       agenda,
       datadesa,
     },
